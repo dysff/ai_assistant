8750948dfeede223cb2e8d40d4caf2bfc52b0ece
Aarthur00 <sivolobov.ilya14@mail.ru>
2024-11-18 22:54:45 +0300

    update

--- main.py
+++ main.py
@@ -1,80 +1,33 @@
-import requests
 import os
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-import openai
-import re
 from PIL import Image, ImageDraw, ImageFont
-from docx import Document
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import Tool
+from langchain_google_community import GoogleSearchAPIWrapper
+from langchain.agents import create_tool_calling_agent
+from langchain.agents import AgentExecutor
+from langchain_core.prompts.chat import MessagesPlaceholder
+from langchain_core.prompts.chat import ChatPromptTemplate
+from langchain_community.agent_toolkits.load_tools import load_tools
+import streamlit as st
+from langchain_community.callbacks import StreamlitCallbackHandler
 
 load_dotenv('.env')
 
 google_api_key = os.getenv('GOOGLE_CSE_API_KEY')
 search_engine = os.getenv('SEARCH_ENGINE_ID')
-openai.api_key = os.getenv('CHAT_GPT_API_KEY')
+openai_api_key = os.getenv('CHAT_GPT_API_KEY')
 
-def search_news(query):
-  url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={google_api_key}&cx={search_engine}"
-  response = requests.get(url).json()
-  output = [item['link'] for item in response['items']]
-    
-  return output
+os.environ["GOOGLE_CSE_ID"] = search_engine
+os.environ["GOOGLE_API_KEY"] = google_api_key
 
-def keep_russian_characters(text): #Drop all html things to save tokens for payload
-  return re.sub(r'[^А-Яа-яЁё ]', '', text)
+search = GoogleSearchAPIWrapper()
 
-def scrape_html(links):
-  scraped_data = ''
-  
-  for link in links:
-      
-    try:
-      url = requests.get(link)
-      htmltext = url.text
-          
-      if 'Access Denied' in htmltext:
-        print(f'ACCESS DENIED FOR A LINK | {link}')
-          
-        continue
-          
-      else:
-        print(f'SCRAPED SUCCESSFUL!      | {link}')
-          
-      soup = BeautifulSoup(htmltext, "html.parser")
-      link_data = ''
-      
-      for element in soup.find_all(['p', 'h1', 'h2', 'h3']):
-        prepared_el = keep_russian_characters(str(element))
-        link_data += prepared_el
-    
-      if len(link_data) > 15000:
-        link_data = link_data[:15000]
-    
-      scraped_data += link_data
-      
-    except requests.exceptions.Timeout:
-      print(f'TIMEOUT ERROR FOR A LINK | {link}')
-          
-      continue
-      
-    except requests.exceptions.RequestException as e:
-      print(f'ERROR FETCHING LINK       | {link} | {e}')
-        
-      continue
-  
-  if len(scraped_data) > 160000:
-    scraped_data = scraped_data[:160000]
-  
-  return scraped_data
-
-def chat_gpt(content, prompt):
-  prompt = f"{prompt}\n\n{content}"
-  response = openai.ChatCompletion.create(
-    model="gpt-4o",
-    messages=[{"role": "user", "content": prompt}],
-  )
-  
-  return response.choices[0].message['content'].strip()
+google_tool = Tool(
+  name="google_search",
+  description="Search Google for recent results.",
+  func=search.run,
+)
 
 def create_banner(size, bgColor, message, font, fontColor):
   W, H = size
@@ -90,27 +43,42 @@ def save_to_txt(text):
     file.write(text)
   
 def main():
-  links = search_news('Важность раннего развития детей')
-  content = scrape_html(links)
+  llm = ChatOpenAI(openai_api_key=openai_api_key, temperature=0.9)
+  system_prompt = 'Create clear blog from the following information provided in Russian without special characters. Divide it into paragraphs to highlight different subtopics'
+
+  prompt_template = ChatPromptTemplate.from_messages(
+    [
+      ("system", system_prompt),
+      MessagesPlaceholder("chat_history", optional=True),
+      ("human", "{input}"),
+      MessagesPlaceholder("agent_scratchpad"),
+    ]
+  )
+    
+  agent = create_tool_calling_agent(llm, [google_tool], prompt_template)
+  agent_executor = AgentExecutor(agent=agent, tools=[google_tool])
   
-  #Generating blog using chatGPT
-  blog_prompt = 'Highlight from this text the main point about the importance of early childhood development, format the resulting answer in the form of a blog in russian language without any special sings(like *, $ etc.).:'
-  blog = chat_gpt(content, blog_prompt)
-  document = Document()
-  document.add_paragraph(blog)
-  document.save('output/blog.docx')
+  st.title("💬 BlogBot")
   
-  #Generating slogan using chatGPT
-  slogan_prompt = 'The main idea or slogan that will be used to create an image (e.g. a banner or infographic) should be extracted from the text. The text should be concise and memorable without any special sings(like *, $ etc.).'
-  slogan = chat_gpt(blog, slogan_prompt)
+  if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+      {"role": "assistant", "content": "Hi, I'm a blogbot who can make blog for any theme. How can I help you?"}
+    ]
   
-  #Making an image using PIL
-  myFont = ImageFont.truetype('C:/Windows/Fonts/Arial.ttf', 30)
-  myImage = create_banner((1000, 700), 'yellow', slogan, myFont, 'black')
-  myImage.save('output/slogan_banner.png', 'PNG')
-  
-  # Writing slogan text to txt file
-  save_to_txt(slogan)
+  for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+    
+  if prompt := st.chat_input(placeholder='Gradient Descent Algorithm'):
+    st.session_state.messages.append({
+      'role': 'user', 'content': prompt
+    })
+    st.chat_message('user').write(prompt)
+    
+    with st.chat_message('assistant'):
+      st_callback = StreamlitCallbackHandler(st.container())
+      response = agent_executor.invoke({'input': 'Найди' + prompt}, {"callbacks": [st_callback]})
+      st.session_state.messages.append({"role": "assistant", "content": response})
+      st.write(response['output'])
   
 if __name__ == '__main__':
   main()
